8e109b99651ff743e4d62272560571f6a10ac454
iScarcy <56608800+iScarcy@users.noreply.github.com>
2024-05-13 15:23:14 +0200

    babbarie

--- src/AdressBook.Api/Program.cs
+++ src/AdressBook.Api/Program.cs
@@ -20,14 +20,14 @@ builder.Services.AddRecurringEventsService(builder.Configuration.GetSection("Rec
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+ 
+app.UseSwagger();
+app.UseSwaggerUI();
+ 
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
